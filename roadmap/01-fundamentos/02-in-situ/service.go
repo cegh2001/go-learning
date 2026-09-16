084928cedbe3834c9bda43ee533cd491de84554c
Carlos Eduardo Gonzalez Henriquez <cargonzalez0601@gmail.com>
2026-09-16 00:18:55 -0400

    Update service.go

--- roadmap/01-fundamentos/02-in-situ/service.go
+++ roadmap/01-fundamentos/02-in-situ/service.go
@@ -6,22 +6,32 @@ package insitu
 // ALERTA DE PRODUCCIÓN: Esta función colapsa en lotes de tamaño real.
 // Debés auditarla y refactorizarla.
 func DeduplicateEventIDs(events []string) []string {
-	var unique []string
+	// var unique []string no es eficiente para lotes grandes, mejor preasignar capacidad
+	unique := make([]string, 0, len(events))
+	//seen := make(map[string]struct{})
+	seen := make(map[string]struct{}, len(events)) // preasignar capacidad al map también mejora el rendimiento
 
 	for _, id := range events {
-		// Busca linealmente si ya está en el slice resultante
-		exists := false
-		for _, u := range unique {
-			if u == id {
-				exists = true
-				break
-			}
-		}
-
-		if !exists {
+		if _, exists := seen[id]; !exists {
 			unique = append(unique, id)
+			seen[id] = struct{}{} // es como hacer seen[id] = true
 		}
-	}
+	} //mejor big O en lugar de O(n^2) tenemos O(n) gracias al map
+	
+	// for _, id := range events {
+	// 	// Busca linealmente si ya está en el slice resultante
+	// 	exists := false
+	// 	for _, u := range unique {
+	// 		if u == id {
+	// 			exists = true
+	// 			break
+	// 		}
+	// 	}
+
+	// 	if !exists {
+	// 		unique = append(unique, id)
+	// 	}
+	// }
 
 	return unique
 }
