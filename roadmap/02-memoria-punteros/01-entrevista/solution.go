cb56ba550b6bcec9419132a8aee219cbe0b9d086
Carlos Eduardo Gonzalez Henriquez <cargonzalez0601@gmail.com>
2026-09-22 23:31:12 -0400

    Update solution.go

--- roadmap/02-memoria-punteros/01-entrevista/solution.go
+++ roadmap/02-memoria-punteros/01-entrevista/solution.go
@@ -8,5 +8,14 @@ type ListNode struct {
 // ReverseList invierte una lista enlazada in-place (reorientando punteros).
 func ReverseList(head *ListNode) *ListNode {
 	// TODO: Implementar inversión in-place O(1) memoria
-	return nil
+	var prev *ListNode
+	curr := head
+
+	for curr != nil {
+		next := curr.Next // 1. Guardo el siguiente nodo para no perderlo
+		curr.Next = prev  // 2. Doy vuelta la flecha (ahora apunto hacia atrás)
+		prev = curr       // 3. Muevo 'prev' un paso adelante
+		curr = next       // 4. Muevo 'curr' un paso adelante
+	}
+	return prev
 }
